e3567915c982c9658e00c65308bb10b8d9889671
赵军 <zhaojun-szgx@bestpay.com.cn>
2023-01-09 12:43:49 +0800

    ip获取修改

--- webSocket/index.js
+++ webSocket/index.js
@@ -5,7 +5,7 @@
  * @Description  :
  * @FilePath     :\项目展示\webSocket\index.js
  * @LastEditors  :赵军
- * @LastEditTime :2022-12-29 22:08:36
+ * @LastEditTime :2023-01-09 12:37:22
  */
 const express = require('express')
 const app = express()
@@ -17,19 +17,25 @@ const io = new Server(server)
 
 const os = require('os')
 function getNetworkIp() {
-  let needHost = '' // 打开的host
+  let needHost = []// 打开的host
   try {
     // 获得网络接口列表
     let network = os.networkInterfaces()
-    let iface = network['本地连接* 10']
-    for (let i = 0; i < iface.length; i++) {
-      let alias = iface[i]
-      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-        needHost = alias.address
-      }
-    }
+    Object.keys(network).forEach((item) => {
+      network[item].forEach((it) => {
+        if (it.family === 'IPv4' && it.address !== '127.0.0.1' && !it.internal) {
+          needHost.push({
+            name: item,
+            address: it.address
+          })
+        }
+      })
+    })
   } catch (e) {
-    needHost = 'localhost'
+     needHost.push({
+            name: 'localhost',
+            address: 'localhost'
+          })
   }
   return needHost
 }
@@ -136,5 +142,9 @@ io.on('connection', (socket) => {
 })
 
 server.listen(3000, () => {
-  console.log(`listening on  http://${ip}:3000`)
+  console.log('listening on');
+  ip.forEach((item) => {
+  console.log(`${item.name} : http://${item.address}:3000`)
+
+  })
 })
