368a559a913051eb80d6e87442987367ea5aca2d
赵军 <1479119670@qq.com>
2024-09-17 16:44:20 +0800

    refactor(index.js): 递归删除目录下所有node_modules文件夹
    添加了新的函数`removeNodeModules`，该函数会递归地删除指定目录下的所有`node_modules`文件夹。该操作对于清理项目中的多余依赖非常有用。
    
    解决了之前需要手动删除嵌套的`node_modules`文件夹的问题，现在可以通过一个函数调用来自动处理。

--- 样式文件，脚本替换/index.js
+++ 样式文件，脚本替换/index.js
@@ -1,9 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
 /**
- * @Author       :赵军
- * @Copyright (c) 2023 by 赵军/公司名, All Rights Reserved.
- * @Date         :2023-03-30 10:57:16
- * @Description  :
- * @FilePath     :\项目展示\样式文件，脚本替换\index.js
- * @LastEditors  :赵军
- * @LastEditTime :2023-03-30 10:57:16
+ * 递归删除指定目录下的所有 node_modules 文件夹
+ * @param {string} dir 当前处理的目录路径
  */
+function removeNodeModules(dir) {
+	const files = fs.readdirSync(dir);
+	files.forEach((file) => {
+		const filePath = path.join(dir, file);
+		const stats = fs.statSync(filePath);
+		if (stats.isDirectory()) {
+			// 如果是目录
+			if (file === "node_modules") {
+				// 如果是 node_modules 目录则删除
+				fs.rmSync(filePath, { recursive: true, force: true });
+			} else if (file !== ".git") {
+				// 否则递归进入下一级目录
+				removeNodeModules(filePath);
+			}
+		}
+	});
+}
+
+// 调用函数，从指定目录开始
+removeNodeModules("D://Users//worktrees//learnhub.worktrees");
