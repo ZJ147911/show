faafe117cca0bdfa1a9946134870e87270a20f06
赵军 <zhaojun-szgx@bestpay.com.cn>
2025-03-19 16:01:18 +0800

    refactor(timeRefresh): 调整页面刷新时间间隔
    - 将页面刷新时间间隔从 10 分钟调整为 5 分钟
    - 更新脚本版本号至 0.0.3

--- tampermonkey/timeRefresh.js
+++ tampermonkey/timeRefresh.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @updateURL    https://gitee.com/zj147/show/raw/zj/tampermonkey/timeRefresh.js
 // @downloadURL  https://gitee.com/zj147/show/raw/zj/tampermonkey/timeRefresh.js
-// @version      0.0.2
+// @version      0.0.3
 // @description  定时刷新脚本
 // @author       zj
 // @match        https://h5performance.tool.bestpay.net/mpaas/*
@@ -24,7 +24,7 @@
 		return `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
 	};
 	// const time = 1000;
-	const time = 1000 * 60 * 10;
+	const time = 1000 * 60 * 5;
 	const reload = () => {
 		console.log('🚀 ~ file: 定时刷新.js:21 ~ time:', dateNow(), time);
 		setTimeout(function () {
